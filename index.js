4dcc72e35c6c5824a97e307a869299536644fb1d
artishok1202 <jager2998@gmail.com>
2025-04-12 16:33:00 +0500

    obnevlanie

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-document.getElementById("speakButton").addEventListener("click", function() {
+document.getElementById("speakButton").addEventListener("click", function () {
     const text = document.getElementById("textInput").value;
-    const language = document.getElementById("languageSelect").value; 
+    const language = document.getElementById("languageSelect").value;
 
     if (text.trim() === "") {
         alert("Iltimos, matn kiriting!");
         return;
     }
 
-    if ('speechSynthesis' in window) {
-        const speech = new SpeechSynthesisUtterance();
-        speech.text = text;
-        speech.lang = language;
+    const speech = new SpeechSynthesisUtterance(text);
+    speech.lang = language;
 
-        speechSynthesis.speak(speech);
-    } else {
-        alert("Brauzeringiz Speech Synthesis API-ni qo'llab-quvvatlamaydi.");
-    }
+    speechSynthesis.speak(speech);
 });
